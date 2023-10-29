aaa4292cdf5910131d314d44db0916a793752864
ggammoh <100084533+ggammoh@users.noreply.github.com>
2023-10-29 18:17:22 -0400

    Update Test.py

--- Test.py
+++ Test.py
@@ -3,8 +3,9 @@ import os
 from gtts import gTTS
 import JarvisCode
 
-#Use this file to create new permant audio files
-file_path = "example.mp3"
+# Use this file to create new permant audio files
+# If you would like to create a personalized start up message you can replace the currant welcome file
+file_path = "Welcome.mp3"
 text = "Input text here"
 
 speech = gTTS(text=text, lang=JarvisCode.lang, slow=False,
